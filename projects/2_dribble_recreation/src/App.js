9b2f79dfb77299da0848000e693117a19543cc38
Joshua <Jplittle2001@gmail.com>
2020-06-07 17:15:23 -0400

    fixed noopener and referrer, proj. 2

--- projects/2_dribble_recreation/src/App.js
+++ projects/2_dribble_recreation/src/App.js
@@ -8,9 +8,9 @@ function App() {
     <div className="App">
       <Page />
       <div className='App-credits'>
-          Remade by: <a href='https://jlittle.dev/' target='_blank'>Joshua Little</a>
+          Remade by: <a href='https://jlittle.dev/' target='_blank' rel='noopener noreferrer'>Joshua Little</a>
           <br /> 
-          Reference: <a href='https://dribbble.com/shots/11580352-Travel-App-Design-Concept' target='_blank'>Purrweb UI on Dribble</a>
+          Reference: <a href='https://dribbble.com/shots/11580352-Travel-App-Design-Concept' target='_blank' rel='noopener noreferrer'>Purrweb UI on Dribble</a>
         </div>
     </div>
   );
